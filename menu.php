7920e595cc127b4d740884293635790564e42c35
Bruno Gomes <brunosoges@gmail.com>
2021-01-17 12:42:18 -0300

    Refatorando o index com o uso do require

---
+++ menu.php
@@ -0,0 +1,128 @@
+<nav class="modulos">
+    <div class="modulo verde">
+        <h3>Seção 3: Integrando PHP com HTML & CSS</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+        </ul>
+    </div>
+    <div class="modulo vermelho">
+        <h3>Seção 4: Tipos Básicos Curso PHP</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=tipos&file=int">Tipo: Inteiros</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=float">Tipo: Float</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Operações Aritméticas</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio: Precedência</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=string">Tipo: String</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio: String</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=boolean">Tipo: boolean</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões</a></li>
+        </ul>
+    </div>
+    <div class="modulo azul">
+        <h3>Seção 5: Variáveis</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=variaveis&file=basico">Variáveis</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio: Equação</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis variáveis</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio: Variáveis</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=valor_referencia">Valor vs Referência</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
+        </ul>
+    </div>
+    <div class="modulo roxo">
+        <h3>Seção 6: Estruturas de controle #1</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=controle&file=if_else">IF / ELSE</a></li>
+            <li><a href="exercicio.php?dir=controle&file=operadores_relacionais">Operadores relacionais</a></li>
+            <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio: PI</a></li>
+            <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores lógicos</a></li>
+            <li><a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio: Operadores lógicos</a>
+            </li>
+            <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador ternário</a></li>
+            <li><a href="exercicio.php?dir=controle&file=switch">Switch</a></li>
+            <li><a href="exercicio.php?dir=controle&file=desafio_switch_1">Desafios: Switch</a></li>
+        </ul>
+    </div>
+    <div class="modulo laranja">
+        <h3>Seção 7: Arrays</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=array&file=basico">Básico sobre array</a></li>
+            <li><a href="exercicio.php?dir=array&file=mapa">Mapa</a></li>
+            <li><a href="exercicio.php?dir=array&file=desafio_index">Desafio: Index</a></li>
+            <li><a href="exercicio.php?dir=array&file=desafio_meses">Desafio: Meses</a></li>
+            <li><a href="exercicio.php?dir=array&file=operacoes">Operações com arrays</a></li>
+            <li><a href="exercicio.php?dir=array&file=desafio_sorteio">Desafio: Sorteio</a></li>
+            <li><a href="exercicio.php?dir=array&file=array_multi">Arrays multidimensionais</a></li>
+            <li><a href="exercicio.php?dir=array&file=array_const">Arrays constantes</a></li>
+            <li><a href="exercicio.php?dir=array&file=get">Usando $_GET</a></li>
+            <li><a href="exercicio.php?dir=array&file=post">Usando $_POST</a></li>
+            <li><a href="exercicio.php?dir=array&file=comparacao">Comparação de arrays</a></li>
+        </ul>
+    </div>
+    <div class="modulo verde-escuro">
+        <h3>Seção 8: Estruturas de controle #2</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=controle2&file=for">Laço FOR</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=desafio_for">Desafio: FOR</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=foreach">FOREACH</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=break_continue">Break / Continue</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=desafio_impressao">Desafio: Impressão</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=while">WHILE / DO WHILE</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=desafio_tabela">Desafio: Tabela #01</a></li>
+            <li><a href="exercicio.php?dir=controle2&file=desafio_tabela2">Desafio: Tabela #02</a></li>
+        </ul>
+    </div>
+    <div class="modulo vermelho-escuro">
+        <h3>Seção 9: Funções</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=funcoes&file=escopo">Função & Escopo</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=argumentos_retorno">Argumentos e retorno</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=desafio_palindromo">Desafio: Palíndromo</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=argumentos_variaveis">Argumentos variáveis</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=argumentos_padrao">Argumentos padrão</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=declarando_tipos">Declarando tipos</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=funcoes_anonimas">Funções anônimas</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=closure_callable">Closure e Callable</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=recursividade">Recursividade</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=desafio_recursividade">Desafio: Recursividade</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=retornando_funcoes">Retornando funções</a></li>
+            <li><a href="exercicio.php?dir=funcoes&file=map_filter">Map e Filter</a></li>
+        </ul>
+    </div>
+    <div class="modulo azul-escuro">
+        <h3>Seção 10: Orientação a Objetos</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=classes_objetos&file=classe">Primeira classe</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=desafio_data">Desafio: Classe data</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=construtor_destrutor">Construtor e destrutor</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=heranca">Herança</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=visibilidade">Modificadores de acesso</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=static">Membros de classe (Static)</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=interface">Interface</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=abstract">Classe abstrata</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros">Desafio: 7 erros</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=modificador_final">Modificador final</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits #01</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=traits_02">Traits #02</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=magic_methods">Métodos mágicos</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=polimorfismo">Polimorfismo</a></li>
+        </ul>
+    </div>
+    <div class="modulo roxo-escuro">
+        <h3>Seção 11: Includes</h3>
+        <ul>
+            <li><a href="exercicio.php?dir=includes&file=include">Arquivo include</a></li>
+            <li><a href="exercicio.php?dir=includes&file=include_funcao">Include função</a></li>
+            <li><a href="exercicio.php?dir=includes&file=include_require">Include vs Require</a></li>
+            <li><a href="exercicio.php?dir=includes&file=require_return">Require com retorno</a></li>
+            <li><a href="exercicio.php?dir=includes&file=include_once">Include/Require once</a></li>
+            <li><a href="exercicio.php?dir=includes&file=include_desafio">Desafio: Include/Require</a></li>
+        </ul>
+    </div>
+</nav>
