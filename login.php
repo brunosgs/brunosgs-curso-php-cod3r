35174a30cf1d20a99fccb99067a1fa84ea0417bf
Bruno Gomes <brunosoges@gmail.com>
2021-01-27 12:41:02 -0300

    Ajustando layout e adicionado login #02

--- login.php
+++ login.php
@@ -1,3 +1,42 @@
+<?php
+session_start();
+
+if(isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $email = $_POST['senha'];
+
+    $usuarios = [
+        [
+            "nome" => "Aluno Cod3r",
+            "email" => "aluno@cod3r.com.br",
+            "senha" => "123456",
+        ],
+        [
+            "nome" => "Outro aluno",
+            "email" => "outro@email.com.br",
+            "senha" => "654321",
+        ]
+    ];
+
+    foreach ($usuarios as $usuario) {
+        $emailValido = $email === $usuario['email'];
+        $senhaValida = $senha === $usuario['senha'];
+
+        if($emailValido && $senhaValida){
+            $_SESSION['erros'] = null;
+            $_SESSION['usuario'] = $usuario['nome'];
+
+            header('Location: index.php');
+        }
+    }
+
+    if(!isset($_SESSION['usuario'])) {
+        $_SESSION['erros'] = ['Usuário/Senha inválido!'];
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -5,21 +44,40 @@
     <meta charset="UTF-8" />
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/login.css" />
     <title>Curso PHP</title>
 </head>
 
-<body>
+<body class="login">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Seja Bem Vindo</h2>
     </header>
     <main class="principal">
         <div class="conteudo">
-            
+            <h3>Identifique-se</h3>
+            <?php if (isset($_SESSION['erros'])) :  ?>
+                <div class="erros">
+                    <?php foreach ($_SESSION['erros'] as $erro) : ?>
+                        <p><?= $erro ?></p>
+                    <?php endforeach ?>
+                </div>
+            <?php endif ?>
+            <form action="#" method="post">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha">
+                </div>
+                <button>Entrar</button>
+            </form>
         </div>
     </main>
     <footer class="rodape">
-        COD3R & ALUNOS &copy <?= date('Y');?>
+        COD3R & ALUNOS &copy <?= date('Y'); ?>
     </footer>
 </body>
 
