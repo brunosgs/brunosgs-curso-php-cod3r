95077bfe74424663531469cfa82ede1de9976a19
Bruno Gomes <brunosoges@gmail.com>
2021-01-26 21:57:44 -0300

    Ajustando layout e adicionando o login #01

--- login.php
+++ login.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-
-if(!$_SESSION['usuario']){
-    header('Location: login.php');
-}
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -19,14 +11,11 @@ if(!$_SESSION['usuario']){
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h2>Seja Bem Vindo</h2>
     </header>
-    <nav class="navegacao">
-        
-    </nav>
     <main class="principal">
         <div class="conteudo">
-            <?php require_once('menu.php'); ?>
+            
         </div>
     </main>
     <footer class="rodape">
