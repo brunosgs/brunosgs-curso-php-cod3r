c69653d5f951ce346a707a28b045360782f0f0a4
Bruno Gomes <brunosoges@gmail.com>
2021-02-15 10:14:20 -0300

    Excluindo registros do banco de dados #02

--- menu.php
+++ menu.php
@@ -175,6 +175,7 @@
             <li><a href="exercicio.php?dir=db&file=inserir_1">Inserir registro #01</a></li>
             <li><a href="exercicio.php?dir=db&file=consultar">Consultar registros</a></li>
             <li><a href="exercicio.php?dir=db&file=excluir_1">Excluir registro #01</a></li>
+            <li><a href="exercicio.php?dir=db&file=excluir_2">Excluir registro #02</a></li>
         </ul>
     </div>
 </nav>
