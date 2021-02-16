dc44c5f2616db81b90a8acc96f8dea55f55d46bd
Bruno Gomes <brunosoges@gmail.com>
2021-02-16 10:43:07 -0300

    Conexao com banco de dados utilizando PDO

---
+++ db/conexao_pdo.php
@@ -0,0 +1,18 @@
+<?php 
+
+function novaConexao($banco = 'curso_php') {
+    $servidor = '127.0.0.1:3307';
+    $usuario = 'root';
+    $senha = 'udemy';
+
+    try {
+        $conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+
+        return $conexao;
+    } catch(PDOException $e) {
+        die('Erro: ' . $e->getMessage());
+    }
+}
+
+novaConexao();
+echo 'Fim!';
