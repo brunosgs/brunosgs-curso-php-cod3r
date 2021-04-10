def5e6bdd5b1023d48fc99ab97679f5c994bf2ee
Bruno Gomes <brunosoges@gmail.com>
2021-04-09 22:10:17 -0300

    Alguns cuidados ao se utilizar Number

---
+++ extras/javascript/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0)
+console.log("10" / 2) // Possível fazer operações com tipo string que contenham números.
+console.log("10,2" / 2)
+console.log("Show!" * 2)
+console.log(0.1 + 0.7) // Gera um imprecisão por causa da especificação da linguagem.
+// console.log(10.toString())
+console.log((10.365).toFixed(2))
+console.log('5' + 3) // Como o operador '+' é utilizado para concatenação, acabou sendo priorizado a contatenação ao invés da soma
