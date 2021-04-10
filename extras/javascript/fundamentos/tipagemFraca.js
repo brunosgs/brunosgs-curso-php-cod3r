040ef54ea0beb667cec575c023d76ab2dba35534
Bruno Gomes <brunosoges@gmail.com>
2021-04-09 21:41:42 -0300

    Tipagem fraca

---
+++ extras/javascript/fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+// A linguem é de tipagem fraca, mas mesmo sendo de tipagem fraca ela possui os tipos.
+let qualquer = 'Legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
